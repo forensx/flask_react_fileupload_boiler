7aced74c7504977fc4c5321f11d6986f295080cb
Aniket Pant <pant.aniket@gmail.com>
2019-12-24 10:37:18 -0600

    boiler for React + Flask API

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Main from './components/FileUpload';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>File Upload</h1>
+      <Main />
+    </div>
+  );
+}
+
+export default App;
